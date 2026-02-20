c297d178d2bd841b8af5717b4443e5821c7b0d07
Johannes Berger <johannes.berger@rwth-aachen.de>
2026-02-20 12:02:08 +0100

    updated packaging setup

--- tabular_examples/setup.py
+++ tabular_examples/setup.py
@@ -1,11 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='custom_envs',
-    version='0.0.1',
-    packages=['custom_envs'],
-    install_requires=['gymnasium'],
-    description='Custom Environments for Reinforcement Learning',
-    author='Bernd Frauenknecht, Emma Cramer, Ramil Sabirov, Lukas Kesper',
-    options={'clean': {'all': True}}
+    name="custom_envs",
+    version="0.0.1",
+    packages=find_packages(include=["custom_envs", "custom_envs.*"]),
+    include_package_data=True,
+    package_data={"custom_envs": ["envs/img/*", "envs/font/*"]},
+    install_requires=["gymnasium"],
+    description="Custom Environments for Reinforcement Learning",
+    author="Bernd Frauenknecht, Emma Cramer, Ramil Sabirov, Lukas Kesper",
+    options={"clean": {"all": True}},
 )
