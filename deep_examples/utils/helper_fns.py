3396cfec1f14a0c049bf7aaa58ffd28d959f2027
Paul Kruse <paul.kruse@posteo.de>
2026-05-24 18:00:02 +0200

    Updated DQN Atari Wrapper Resulted in more stable training

--- deep_examples/utils/helper_fns.py
+++ deep_examples/utils/helper_fns.py
@@ -15,7 +15,7 @@ import gymnasium as gym
 import wandb
 import torch
 from stable_baselines3.common.atari_wrappers import MaxAndSkipEnv, EpisodicLifeEnv, FireResetEnv, \
-    ClipRewardEnv
+    ClipRewardEnv, NoopResetEnv
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=DeprecationWarning)
@@ -35,36 +35,6 @@ Here is a list of important helper functions (in no particular order). You may c
 7. 'make_atari_env' - helps in creating a vectorized gymnasium environment for the Breakeout Atari game
 '''
 
-
-class NoopResetEnv(gym.Wrapper):
-    """
-    Sample initial states by taking random number of no-ops on reset.
-    No-op is assumed to be action 0.
-
-    :param env: the environment to wrap
-    :param noop_max: the maximum value of no-ops to run
-    """
-
-    def __init__(self, env: gym.Env, noop_max: int = 30):
-        gym.Wrapper.__init__(self, env)
-        self.noop_max = noop_max
-        self.override_num_noops = None
-        self.noop_action = 0
-        assert env.unwrapped.get_action_meanings()[0] == "NOOP"
-
-    def reset(self, **kwargs) -> np.ndarray:
-        self.env.reset(**kwargs)
-        if self.override_num_noops is not None:
-            noops = self.override_num_noops
-        else:
-            noops = self.unwrapped.np_random.integers(1, self.noop_max + 1)
-        assert noops > 0
-        obs = np.zeros(0)
-        for _ in range(noops):
-            obs, _, terminated,truncated, _ = self.env.step(self.noop_action)
-            if terminated | truncated:
-                obs, _ = self.env.reset(**kwargs)
-        return obs, {}
 
 def make_single_env(env_id, seed):
     """
@@ -106,16 +76,21 @@ def make_env(env_id, seed):
 
 def make_atari_breakout_env(env_id, seed):
     def thunk():
-
         env = gym.make(env_id)
-        env = gym.wrappers.AtariPreprocessing(env, frame_skip=4, grayscale_obs=True, scale_obs=False, terminal_on_life_loss=False)
-        env = gym.wrappers.FrameStack(env, 4)
-        env = ClipRewardEnv(env)
         env = gym.wrappers.RecordEpisodeStatistics(env)
-        env.action_space.seed(seed)
-        env.observation_space.seed(seed)
-        np.random.seed(seed)
+        env = NoopResetEnv(env, noop_max=30)
+        env = MaxAndSkipEnv(env, skip=4)
+        env = EpisodicLifeEnv(env)
 
+        if "FIRE" in env.unwrapped.get_action_meanings():
+            env = FireResetEnv(env)
+
+        env = ClipRewardEnv(env)
+        env = gym.wrappers.ResizeObservation(env, (84, 84))
+        env = gym.wrappers.GrayScaleObservation(env)
+        env = gym.wrappers.FrameStack(env, 4)
+
+        env.action_space.seed(seed)
         return env
 
     return thunk
